3c4b99f82e0e75a4c234f0d9ff61d48575841d3c
KENNIER ARMANDO LUJAN GARCIA <KLUJAN@INTERNEXA.COM>
2022-08-01 22:28:52 -0500

    Entregable Kanban App

---
+++ js/helpers/newTask.js
@@ -0,0 +1,25 @@
+// Apuntamos al formulario para crear la nueva tarea del HTML
+const form = document.querySelector('#formNewTask');
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const formData = ev.target;
+
+    const days = FormData.titleTask.value;
+
+    const data = {
+        title: formData.titleTask.value,
+        person: formData.responsibleTask.value,
+        details: formData.detailsTask.value,
+        deadline: Number(moment().add(formData.deadLineTask.value, 'days').format('X')),
+        created: Number(moment().format("X")),
+        state: 'to-do'
+    };
+
+    axios.post(`${API_URL}/tasks`, data)
+    .then((res) => {
+        createTask(res.data);
+        formData.reset();
+    })
+    .catch((err) => console.error(err));
+});
